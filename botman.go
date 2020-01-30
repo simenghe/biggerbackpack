bbd9653bec5412fba0d97c85423772af23823572
Simeng He <simengschool@gmail.com>
2020-01-29 19:09:39 -0500

    eeee bigger backpack

---
+++ botman.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Bag struct {
+	Title string  `json:"Title"`
+	Desc  string  `json:"Desc"`
+	Price float64 `json:"Price"`
+}
+
+var Bags []Bag
+
+func fetchBags(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Fetching Bags")
+	bags := []Bag{
+		Bag{Title: "Jansport", Desc: "eLITE women", Price: 32.3},
+		Bag{Title: "Applyboard", Desc: "eLITE man", Price: 12.3},
+	}
+	json.NewEncoder(w).Encode(bags)
+}
+func handleReq() {
+	fmt.Println("Handling the requests!")
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", fetchBags)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func main() {
+	fmt.Println("Main is running")
+	handleReq()
+}
