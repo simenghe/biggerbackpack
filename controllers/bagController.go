a1f8af94075b8f7845c7fe89a6da63a6447de274
simeng he <simengschool@gmail.com>
2020-02-01 20:55:50 -0500

    steal my sql

--- controllers/bagController.go
+++ controllers/bagController.go
@@ -1,17 +1,11 @@
 package controllers
 
-import(
+import (
+	"Backpack/core"
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
-	"Backpack/core"
 )
-// Bag Type For Elites
-// type Bag struct {
-// 	Title string  `json:"Title"`
-// 	Desc  string  `json:"Desc"`
-// 	Price float64 `json:"Price"`
-// }
 
 // FetchBags Returns the bags data to the mandems
 func FetchBags(w http.ResponseWriter, r *http.Request) {
