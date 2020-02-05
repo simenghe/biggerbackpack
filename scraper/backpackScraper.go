e066533bc9016af6c449d84a2cddc60f6f97c6ed
simenghe-applyboard <simeng.he@applyboard.com>
2020-02-05 11:17:47 -0500

    more scraper changes

--- scraper/backpackScraper.go
+++ scraper/backpackScraper.go
@@ -1,31 +1,23 @@
 package main
 
 import (
+	"Backpack/utils"
 	"fmt"
-	"regexp"
 
 	"github.com/gocolly/colly"
 )
 
-func FormatPrice(price *string) {
-	r := regexp.MustCompile(`\$(\d+(\.\d+)?).*$`)
-
-	newPrices := r.FindStringSubmatch(*price)
-
-	if len(newPrices) > 1 {
-		*price = newPrices[1]
-	} else {
-		*price = "Unknown"
-	}
-
+// Item has the fields for a basic amazon catalogue item
+type Item struct {
+	Name   string  `json:"Name"`
+	Desc   string  `json:"Desc"`
+	Price  float64 `json:"Price"`
+	Rating float32 `json:"Rating"`
 }
 
-func FormatStars(stars *string) {
-	if len(*stars) >= 3 {
-		*stars = (*stars)[0:3]
-	} else {
-		*stars = "Unknown"
-	}
+// ScrapeAmazon returns an array of Items from amazon.
+func ScrapeAmazon() []Item {
+	return nil
 }
 func main() {
 	c := colly.NewCollector()
@@ -37,7 +29,6 @@ func main() {
 	c.OnHTML("div.s-result-list.s-search-results.sg-row", func(e *colly.HTMLElement) {
 		e.ForEach("div.a-section.a-spacing-medium", func(_ int, e *colly.HTMLElement) {
 			var productName, stars, price string
-
 			productName = e.ChildText("span.a-size-base-plus.a-color-base.a-text-normal")
 			if productName == "" {
 				// If we can't get any name, we return and go directly to the next element
@@ -45,11 +36,11 @@ func main() {
 			}
 			stars = e.ChildText("span.a-icon-alt")
 			price = e.ChildText("span.a-price > span.a-offscreen")
-			FormatStars(&stars)
-			// FormatPrice(&price)
+			utils.FormatStars(&stars)
+			// utils.FormatPrice(&price)
 			fmt.Printf("Product Name: %s \nStars: %s \nPrice: %s \n", productName, stars, price)
 		})
 	})
 
-	c.Visit("https://www.amazon.ca/s?k=backpack&ref=nb_sb_noss_2")
+	c.Visit("https://www.amazon.ca/s?k=backpack&ref=nb_sb_noss_1")
 }
