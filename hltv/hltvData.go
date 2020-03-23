3c6dc52d45c780c7c168c44ad0d24cd3a32ead51
Simeng He <simengschool@gmail.com>
2020-03-22 22:45:59 -0400

    temp not using db

--- hltv/hltvData.go
+++ hltv/hltvData.go
@@ -1,9 +1,9 @@
 package hltv
 
 import (
-	"database/sql"
-	"fmt"
 	hltvScraper "Backpack/scraper"
+	"fmt"
+
 	// "encoding/json"
 	// MySql Database Driver
 	_ "github.com/go-sql-driver/mysql"
@@ -11,25 +11,26 @@ import (
 
 // GetTeamData grabs bags from MySQL database
 func GetTeamData() []hltvScraper.CSGOteam {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3308)/backpack")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-	results, err := db.Query("SELECT * FROM csgoteams")
-	if err != nil {
-		panic(err.Error())
-	}
-	csgoTeams := []hltvScraper.CSGOteam{}
-	for results.Next() {
-		var team hltvScraper.CSGOteam
-		err = results.Scan(&team.ID, &team.TeamName, &team.Ranking, &team.URL, &team.Date)
-		if err != nil {
-			panic(err.Error())
-		}
-		csgoTeams = append(csgoTeams, team)
-	}
-	fmt.Println("Successful connection to MySQL.")
+	// db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3308)/backpack")
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// defer db.Close()
+	// results, err := db.Query("SELECT * FROM csgoteams")
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// csgoTeams := []hltvScraper.CSGOteam{}
+	// for results.Next() {
+	// 	var team hltvScraper.CSGOteam
+	// 	err = results.Scan(&team.ID, &team.TeamName, &team.Ranking, &team.URL, &team.Date)
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
+	// 	csgoTeams = append(csgoTeams, team)
+	// }
+	// fmt.Println("Successful connection to MySQL.")
+	csgoTeams := hltvScraper.ScrapeHltvTeams()
 	fmt.Println(csgoTeams)
 	return csgoTeams
 }
--- scraper/hltvScraper.go
+++ scraper/hltvScraper.go
@@ -4,27 +4,31 @@ import (
 	"fmt"
 	"strings"
 
+	"strconv"
+	"time"
+
 	"github.com/gocolly/colly"
 	// "regexp"
 )
 
 // CSGOteam holds the information for the csgo team
 type CSGOteam struct {
-	ID       int    `json:"ID"`
-	TeamName string `json:"team_name"`
-	Points   int    `json:"points"`
-	Ranking  string `json:"ranking"`
-	URL      string `json:"url"`
-	Date     string `json:"date"`
-	Player1  string `json:"player_1"`
-	Player2  string `json:"player_2"`
-	Player3  string `json:"player_3"`
-	Player4  string `json:"player_4"`
-	Player5  string `json:"player_5"`
+	ID         int    `json:"ID"`
+	TeamName   string `json:"team_name"`
+	Points     string `json:"points"` // Points need to be int
+	Ranking    int    `json:"ranking"`
+	URL        string `json:"url"`
+	Date       string `json:"date"`
+	PlayerList string `json:"player_list"`
+	// Player1  string `json:"player_1"`
+	// Player2  string `json:"player_2"`
+	// Player3  string `json:"player_3"`
+	// Player4  string `json:"player_4"`
+	// Player5  string `json:"player_5"`
 }
 
 // ScrapeHltvTeams scrapes the top teams, needs automation currently.
-func ScrapeHltvTeams() {
+func ScrapeHltvTeams() []CSGOteam {
 	url := "https://www.hltv.org/ranking/teams"
 	c := colly.NewCollector()
 	var header = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1913.47 Safari/537.36"
@@ -35,19 +39,32 @@ func ScrapeHltvTeams() {
 		r.Headers.Set("User-Agent", header)
 
 	})
+	var csgoteams = []CSGOteam{}
 	c.OnHTML("div.ranked-team", func(e *colly.HTMLElement) {
 		e.ForEach("div.ranking-header", func(_ int, e *colly.HTMLElement) {
-			fmt.Println(e.ChildText("span.position"))
-			fmt.Println(e.ChildText("span.name"))
-			fmt.Println(e.ChildText("span.points"))
-
 			playerString := e.ChildText("div.playersLine")
 			players := strings.Split(playerString, "\n")
 			for i := range players {
 				players[i] = strings.TrimSpace(players[i])
 			}
-			fmt.Println(players)
+			const layoutISO = "2006-01-02" // Format the date for golang
+			ranking, err := strconv.Atoi(strings.ReplaceAll(e.ChildText("span.position"), "#", ""))
+			if err != nil {
+				ranking = 0
+			}
+			var team = CSGOteam{
+				TeamName:   e.ChildText("span.name"),
+				Ranking:    ranking,
+				Points:     e.ChildText("span.points"),
+				PlayerList: strings.Join(players, " "),
+				Date:       time.Now().Format(layoutISO),
+			}
+			// fmt.Println(players)
+			csgoteams = append(csgoteams, team)
 		})
+		// Hard assign the players
 	})
 	c.Visit(url)
+	fmt.Println(csgoteams)
+	return csgoteams
 }
