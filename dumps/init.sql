a6fd3003565da0f39a0fdc18589251a6f19f8efa
Simeng He <simengschool@gmail.com>
2020-03-22 22:50:39 -0400

    rats

--- dumps/init.sql
+++ dumps/init.sql
@@ -1,15 +1,21 @@
 DROP DATABASE IF EXISTS backpack;
 CREATE DATABASE backpack;
 USE backpack;
-CREATE TABLE csgoteams(
-  id INT AUTO_INCREMENT,
-  team_name VARCHAR(100),
+CREATE TABLE csgoteams
+(
+  id INT
+  AUTO_INCREMENT,
+  team_name VARCHAR
+  (100),
   ranking INT,
-  url VARCHAR(100),
-  fetch_date DATETIME,
-  PRIMARY KEY(id)
+  url VARCHAR
+  (100),
+  fetch_date DATE,
+  PRIMARY KEY
+  (id)
 );
--- Astralis Example
-INSERT INTO csgoteams (team_name, ranking, url, fetch_date)
-values
-  ('Astralis', 1, 'hltv', now());
+  -- Astralis Example
+  INSERT INTO csgoteams
+    (team_name, ranking, url, fetch_date)
+  values
+    ('Astralis', 1, 'hltv', now());
